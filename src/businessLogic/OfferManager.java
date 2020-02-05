861cf8ed2bb9c363744511c345e4b1f0e9181cfe
jon <jon.iturrioz@ehu.es>
2020-02-05 15:47:46 +0100

    updated

--- src/businessLogic/OfferManager.java
+++ src/businessLogic/OfferManager.java
@@ -3,9 +3,6 @@ package businessLogic;
 import java.util.Collection;
 import java.util.Date;
 
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-
 import domain.Offer;
   
 public interface OfferManager { 
